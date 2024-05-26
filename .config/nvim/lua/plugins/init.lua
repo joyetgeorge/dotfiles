c7e1fb79d33bb01104162f240fe9f99998d04749
Joyet George <joyetgeorge07@gamil.com>
2024-05-26 14:58:08 +0530

    nvim: Updated nvim config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,18 +1,17 @@
 return {
-  {
-    "ggandor/leap.nvim",
-    enabled = true,
-    keys = {
-      { "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
-      { "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
-      { "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
-    },
-    config = function(_, opts)
-      local leap = require "leap"
-      leap.add_default_mappings(true)
-    end,
-  },
-
+  -- {
+  --   "ggandor/leap.nvim",
+  --   enabled = true,
+  --   keys = {
+  --     { "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
+  --     { "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
+  --     { "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
+  --   },
+  --   config = function(_, opts)
+  --     local leap = require "leap"
+  --     leap.add_default_mappings(true)
+  --   end,
+  -- },
   {
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
