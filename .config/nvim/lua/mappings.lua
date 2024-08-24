e738507360fcfb9213d6fbd70df2c6c46ca638b6
Joyet George <joyetgeorge@gamil.com>
2024-08-24 23:38:51 +0530

    update dots
    Signed-off-by: Joyet George <joyetgeorge@gamil.com>

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,12 +1,14 @@
-require "nvchad.mappings"
-
--- add yours here
-
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-map({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
-map({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
-map({ "n", "x", "o" }, "ls", "<Plug>(leap-from-window)")
+local function pick_dropbar()
+  require('dropbar.api').pick()
+end
+
+map('n', ';', ':', { desc = 'CMD enter command mode' })
+map('i', 'jk', '<ESC>')
+map('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+map('n', '<leader>e', '<CMD>:NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
+map('n', '<leader>dp', pick_dropbar, { desc = 'Dropbar Pick' })
+map({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+map({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+map({ 'n', 'x', 'o' }, 'ls', '<Plug>(leap-from-window)')
