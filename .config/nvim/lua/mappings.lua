80e1cbc8a37e1ba29611fd05b4839fdf3b9372a9
Joyet George <joyetgeorge@gmail.com>
2026-01-11 01:11:16 +0530

    config update
    Signed-off-by: Joyet George <joyetgeorge@gmail.com>

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Option+Backspace to delete word backward
+map("i", "<A-BS>", "<C-w>", { desc = "Delete word backward" })
+map("c", "<A-BS>", "<C-w>", { desc = "Delete word backward in command line" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
