1f9d5e98b02180ef91b56e8d6f467db866e4ffe2
Joyet George <joyetgeorge@gmail.com>
2026-01-11 13:37:44 +0530

    feat: add yank hilighting
    Signed-off-by: Joyet George <joyetgeorge@gmail.com>

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,5 +1,13 @@
 require "nvchad.autocmds"
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- -- Quit Neovim when closing the last buffer
 -- vim.api.nvim_create_autocmd("BufDelete", {
 --   callback = function()
